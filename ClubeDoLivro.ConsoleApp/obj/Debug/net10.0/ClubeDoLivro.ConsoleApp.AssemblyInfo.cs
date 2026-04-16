56c0416e11ccdcaf146bf7fb2cdecc62dd746802
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-04-16 13:40:38 -0300

    organizando o codigo

--- ClubeDoLivro.ConsoleApp/obj/Debug/net10.0/ClubeDoLivro.ConsoleApp.AssemblyInfo.cs
+++ ClubeDoLivro.ConsoleApp/obj/Debug/net10.0/ClubeDoLivro.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClubeDoLivro.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+876735264d4bf8df3050d337ae023d41b935d5fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56b2f73987aa8271d1b237c89abba8a5592f4779")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClubeDoLivro.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClubeDoLivro.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
